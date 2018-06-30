62c2b3ad87822dcabf51ac74575743e10ea372f5
vitorhugoclz <37125102+vitorhugoclz@users.noreply.github.com>
2018-06-30 14:40:14 -0300

    Banner
    Foi colocado um banner no código com efeito de foto-legenda
    E foram removidas algumas linhas do código que estavam em desuso

---
+++ header.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="utf-8" />
+    <link rel="icon" href="http://getbootstrap.com/favicon.ico" />
+
+    <title>Lab LInC</title>
+
+    <!-- Bootstrap core CSS
+		Adiciona principal arquivo css bootstrap (base externo)
+	-->
+    <link href="css/bootstrap.css" rel="stylesheet" />
+
+	<!-- Ajusta o padding inferior da navabar -->
+    <link href="css/ajuste_menu.css" rel="stylesheet" />
+
+    <!-- ajustes de imagem com legenda (banner) -->
+    <link href="css/index_estilo.css" rel="stylesheet" />
+
+  </head>
+
+  <body>	
+	    <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+	  <a class="navbar-brand" target="_blank" href="http://www.bcc.unifal-mg.edu.br/portal/">
+	  <img class="d-block mx-auto" id="bcc-logo" src="fotos/bcc.jpg"/> <!--logo do Bcc-->
+        </a>
+      <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="navbar-collapse collapse" id="navbarsExampleDefault" style="">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+			<a class="nav-link" href="index.php">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="equipe.php"> Equipe </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="projetos.php"> Projetos </a>
+          </li>
+		  <li class="nav-item">
+            <a class="nav-link" href="artigos.php"> Artigos </a>
+          </li>
+		  <li class="nav-item">
+            <a class="nav-link" href="contato.php"> Contato </a>
+          </li>
+        </ul>
+      </div>
+	  <a class="py-2 d-none d-md-inline-block"  target="_blank" href="http://www.unifal-mg.edu.br/portal/">
+			<img class="d-block mx-auto" id="unifal-lgo" src="fotos/unifal.jpg" />
+		</a>
+    </nav>
+    <!-- Conteudo inicio -->
--- index.php
+++ index.php
@@ -6,7 +6,14 @@ include("include/header.php");
 		<!-- Example row of columns -->
 		<div class="row">
 			<div class="col-md-12"> <!-- determina 12 colunas para o texto, usado para responsividade -->
-				<h2>Teste para aparecer texto na pagina</h2>
+                <div class="container-fluid">
+                        <figure class="figure">
+                            <img  src="fotos/banner_texto.jpg" class="figure-img img-fluid rounded" />
+                            <figcaption class="figure-caption">
+                                <p>O Laboratório de Inteligência Computacional da Universidade Federal de Alfenas (LInC) foi criado em 2010 com o objetivo de desenvolver pesquisas na área Inteligência Artificial (IA).</br> Desde então, o foco tem sido estudar os diversos algoritmos inteligentes existentes, propondo novas abordagens e com isso desenvolvendo sistemas inteligentes capazes de resolver problemas existentes na sociedade. No âmbito geral, o LInC trabalha focado no desenvolvimento da Ciência da Computação conbribuindo para formação de recursos humanos capazes de utilizar a IA como ferramenta na solução de problemas práticos.</p>
+                            </figcaption>
+                        </figure>
+                </div>
 			</div>
 		</div>
 		<hr>
