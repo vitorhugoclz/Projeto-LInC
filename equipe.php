8f54c721fd24d565c7dd114369ddfd62ede358ad
vitorhugoclz <37125102+vitorhugoclz@users.noreply.github.com>
2018-07-02 22:23:00 -0300

    Equipe (semi-feito)
    Foi feito um design básico para a pagina das equipe e ajustes de responsividade para diferentes dispositivos

--- equipe.php
+++ equipe.php
@@ -3,31 +3,125 @@ include("include/header.php");
 ?>
 <main role="main">
 	<div class="container">
-		<!-- Example row of columns -->
-		<div class="row">
-			<div class="col-md-12"> <!-- determina 12 colunas para o texto, usado para responsividade -->
-				<!-- bloco onde se encontram os membro do LInC ser uma bloco a parte -->
-				<h1>Membros Ativos:</h1>
-					<h2>Times 1: </h2>
-						<p id="participantes">	Luis Gustavo (Capitão) </p> <!--coloca mesma formatação para o participantes pelo ID-->
-						<p id="participantes">Nathalia Machado </p>
-						<p id="participantes">Thiago Dias  </p>
-						<p id="participantes">Pedro Afonso</p>
-						<!-- inserir tambem projetos em que estão participando e periodo -->
-					<h2>Time 2:</h2>
-							<p id="participantes">Daniel <!-- Capitão ??--> </p>
-							<p id="participantes">Pedro Kiyuna </p>
-							<p id="participantes">Rafael </p>
-							<p id="participantes">Felipe Matins </p>
-						<h3>Orientador: <b>Ricardo Menezes</b></h3>
+		<div class="row align-items-center">
+            <div class="col-md-12" align="center">
+            <h1>Orientadores: </h1> <br/>
+            </div>
+			<div class="col-md-4" align="right"> <!-- coloca a foto com legenda em 4 colunas e deslocamento a direita -->
+                        <figure class="orientador">
+                            <img src="fotos/pessoas/ricardo.jpeg" class="img-thumbnail"/> <!-- foto pequena -->
+                            <figcaption class="desc_orientador"> <!-- coloca uma legenda na foto com o nome da pessoa -->
+                                <h2>Ricardo Menezes Salgado</h2>
+                            </figcaption>
+                        </figure>
+            </div>
+            <div class="col-md-2" align="left"> <!-- coloca o texto sobre a pessoa na lateal da foto e em 2 colunas -->
+                <h3 class="titulacao">Titulação: </h3>
+                <p class="descricao">
+                    Doutor em Engenharia Elétrica pela Faculdade de Engenharia Elétrica e Computação da Universidade Estadual de Campinas (2009).
+                </p>
+                <a target="_blank" href="http://buscatextual.cnpq.br/buscatextual/visualizacv.do?id=K4762367Y2">MAIS</a>
+                <!--link acima para o curiculo dos professor -->
+            </div>
+            <div class="col-md-4" align="right"> <!-- coloca a foto com legenda em 4 colunas e deslocamento a direita -->
+                <figure class="orientador">
+                    <img src="fotos/pessoas/luisim.jpeg" class="img-thumbnail"/> <!-- foto pequena -->
+                    <figcaption> <!-- coloca uma legenda na foto com o nome da pessoa -->
+                        <h2>Luis Eduardo da Silva</h2>
+                    </figcaption>
+                </figure>
+            </div>
+            <div class="col-md-2" align="left"> <!-- coloca o texto sobre a pessoa na lateal da foto e em 2 colunas -->
+                <h3 class="titulacao">Titulação:</h3>
+                <p class="descricao">
+                    Doutor em Ciências em Engenharia Elétrica pela Universidade Federal de Itajubá (2012).
+                </p>
+                <a target="_blank" href="http://buscatextual.cnpq.br/buscatextual/visualizacv.do?id=K4767518A1">MAIS</a>
+                <!--link acima para o curiculo dos professor -->
+            </div>
+            <div class="col-md-12">
+				<h1>Discentes: </h1>
+            </div>
+            <div class="col-md-12" align="left">
+					<h2 class="time1">Time: </h2>
+            </div>
+            <!-- identação para relacao de pertinencia visual -->
+                <div class="col-md-3">
+                    <figure class="discentes">
+                        <img src="fotos/pessoas/masculino.jpg" class="img-thumbnail" />
+                            <figcaption>
+                                <h3>Luis Gustavo</h3>
+                            </figcaption>
+                    </figure>
+                </div>
+                <div class="col-md-3">
+                    <figure class="discentes">
+                        <img src="fotos/pessoas/feminino.jpg" class="img-thumbnail" />
+                        <figcaption>
+                            <h3>Nathalia Machado</h3>
+                        </figcaption>
+                    </figure>
+                </div>
+                <div class="col-md-3" align="left">
+                    <figure class="discentes">
+                        <img src="fotos/pessoas/masculino.jpg" class="img-thumbnail" />
+                        <figcaption>
+                            <h3>Thiago Dias</h3>
+                        </figcaption>
+                    </figure>
+                </div>
+                <div class="col-md-3" align="left">
+                    <figure class="discentes">
+                        <img src="fotos/pessoas/masculino.jpg" class="img-thumbnail" />
+                        <figcaption>
+                            <h3>Pedro Afonso</h3>
+                        </figcaption>
+                    </figure>
+                </div>
 
-						<!-- inserir tambem projetos em que estão participando e periodo -->
-					<h2>Membros Inativos:</h2>
+            <div class="col-md-12" align="left">
+                <h2 class="time2">Time:</h2>
+            </div>
+            <!-- indentação para estabelecer relação de pertinencia -->
+                <div class="col-md-3" align="left">
+                    <figure class="discentes">
+                        <img src="fotos/pessoas/masculino.jpg" class="img-thumbnail" />
+                        <figcaption>
+                            <h3>Daniel</h3>
+                        </figcaption>
+                    </figure>
+                </div>
+                <div class="col-md-3" align="left">
+                    <figure class="discentes">
+                        <img src="fotos/pessoas/masculino.jpg" class="img-thumbnail" />
+                        <figcaption>
+                            <h3>Pedro Kiyuna</h3>
+                        </figcaption>
+                    </figure>
+                </div>
+                <div class="col-md-3" align="left">
+                    <figure class="discentes">
+                        <img src="fotos/pessoas/masculino.jpg" class="img-thumbnail" />
+                        <figcaption>
+                            <h3>Rafael</h3>
+                        </figcaption>
+                    </figure>
+                </div>
+                <div class="col-md-3" align="left">
+                    <figure class="discentes">
+                        <img src="fotos/pessoas/masculino.jpg" class="img-thumbnail" />
+                        <figcaption>
+                            <h3>Felipe Martins</h3>
+                        </figcaption>
+                    </figure>
+                </div>
+
+					<h2>Ex-integrantes: </h2>
 						<!-- Inserir Ex-participantes do LInc e projetos em que atuaram -->
 			</div>
 		</div>
 		<hr>
-	</div> <!-- /container -->
+    <!-- /container -->
 </main>
 <?php
 include("include/footer.php");
