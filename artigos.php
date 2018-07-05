43afbc946a5d16d4e45805adda03d0e6c51cb670
vitorhugoclz <37125102+vitorhugoclz@users.noreply.github.com>
2018-07-05 15:37:53 -0300

    feito artigos
    foi feito um design prévio da parte de artigo e foi subido linck de artigo aos pdf

--- artigos.php
+++ artigos.php
@@ -6,8 +6,26 @@ include("include/header.php"); #inclui o cabeçalho do site que foi criado a par
 		<!-- Example row of columns -->
 		<div class="row">
 			<div class="col-md-12"> <!-- determina o texto ocupando 12 colunas que dividem a tela -->
-				<!--bloco de textos -->
-				<p>Teste para aparecer texto na pagina</p>
+                <!-- coloca todos os links de artigos dentro de uma tabela -->
+                <table class="table" cellpadding="0" cellspacing="0" align="center" width="100%" height="100%">
+                    <tr>
+                        <a class="artigo_nome" href="PDF_Artigos/1.Analysis_of_Two-Phase_Flow_Pattern_Identification_Methodologies_for_Embedded_Systems.pdf" target="_blank">Analysis of Two-Phase Flow Pattern Identification Methodologies for Embedded Systems</a><br>
+                        <a class="artigo_nome" href="PDF_Artigos/2.An_Intelligent_Model_to_Assist_Customers_to.pdf" target="_blank">An Intelligent Model to Assist Customers</a><br>
+                        <a class="artigo_nome" href="PDF_Artigos/3.Intelligent_Modeling_for%20_Streamflow_Forecasting.pdf" target="_blank"> 	Intelligent Modeling for Streamflow Forecastin</a><br/>
+                        <a class="artigo_nome" href="PDF_Artigos/4.Intelligent_Models_to_Identification_and.pdf" target="_blank">Intelligent Models to Identification and</a><br/>
+                        <a class="artigo_nome" href="PDF_Artigos/5.MAX-MIN_paraconsistent_ant_algorithm.pdf" target="_blank">MAX-MIN paraconsistent ant algorithm</a><br/>
+                        <a class="artigo_nome" href="PDF_Artigos/7.A_Hybrid_Approach_to_the_Load_Forecasting_Based.pdf" target="_blank">A Hybrid Approach to the Load Forecasting Based</a><br/>
+                        <a class="artigo_nome" href="PDF_Artigos/8.An_Intelligent_Decision.pdf" target="_blank">An Intelligent Decision</a><br/>
+                        <a class="artigo_nome" href="PDF_Artigos/9.Modelo_inteligente_para_previsão_do_preço_diário_ da_saca_do_café_baseado_em_combinação_de_previsores.pdf">Modelo inteligente para previsão do preço diário da saca do café baseado em combinação de previsores</a><br/>
+                        <a class="artigo_nome" href="PDF_Artigos/10.Um_modelo_inteligente_para_previsão_diária_do_preço_da_arroba_do_boi_gordo.pdf" target="_blank">Um modelo inteligente para previsão diária do preço da arroba do boi gordo</a><br/>
+                        <a class="artigo_nome" href="PDF_Artigos/11.Modelos_de_Inteligência_Computacional_para_Geração_de_Séries_Sintéticas _de_Vazões_Médias_Mensais.pdf" target="_blank">Modelos de Inteligência Computacional para Geração de Séries Sintéticas de Vazões Médias Mensais</a><br/>
+                        <a class="artigo_nome" href="PDF_Artigos/12.A_Short-Term_Bus_Load_Forecasting_System.pdf" target="_blank">A Short-Term Bus Load Forecasting System</a><br/>
+                        <a class="artigo_nome" href="PDF_Artigos/13.An _Empirical_Analysis_of_MLP_Neural.pdf" target="_blank">An Empirical Analysis of MLP Neural</a><br/>
+                        <a class="artigo_nome" href="PDF_Artigos/14.MODELO_BASEADO_EM_COMBINAÇÃO_DE_PREVISORES_PARA_PREVISÃO_DE_SÉRIES_TEMPORAIS_DE_CARGA_ELÉTRICA-GEINTEC.pdf" target="_blank">MODELO BASEADO EM COMBINAÇÃO DE PREVISORES PARA PREVISÃO DE SÉRIES TEMPORAIS DE CARGA ELÉTRICA.GEINTEC</a><br/>
+                        <a class="artigo_nome" href="PDF_Artigos/15.Performance_comparison_of_artificial_neural_networks_and_expert_systems_applied.pdf" target="_blank">Performance comparison of artificial neural networks and expert systems applied</a><br/>
+                        <a class="artigo_nome" href="PDF_Artigos/17.Controle_de_inércia_não_monotônico_na_otimização.pdf" target="_blank">Controle de inércia não monotônico na otimização</a><br/>
+                    </tr>
+                </table>
 			</div>
 		</div>
 		<hr>
@@ -17,3 +35,5 @@ include("include/header.php"); #inclui o cabeçalho do site que foi criado a par
 include("include/footer.php");
 ?>
 
+
+<!--<td><iframe src="PDF_Artigos/1.Analysis_of_Two-Phase_Flow_Pattern_Identification_Methodologies_for_Embedded_Systems.pdf" width="600" height="780" style="border: none;"></iframe></td>
